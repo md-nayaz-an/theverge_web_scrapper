329707e98b62598c8b51d5608721f0ac9e5bf85d
nayaz <mohammednayazan@gmail.com>
2023-03-31 22:01:31 +0530

    ec2 running with log

--- verge_scraper.py
+++ verge_scraper.py
@@ -1,37 +1,25 @@
-<<<<<<< HEAD
-from distutils.file_util import write_file
-import requests, bs4
-from selenium import webdriver
-from regex import Regex
-=======
-import requests, bs4, csv, sqlite3
+import requests, bs4, csv, sqlite3, pytz
 from selenium import webdriver
 from regex import Regex
 from datetime import datetime
-
->>>>>>> main
+from selenium.webdriver.firefox.options import Options
 
 re = Regex()
+ist = pytz.timezone('Asia/Kolkata')
 
 class scraper():
   url = ""
   res = ''
-<<<<<<< HEAD
-
-  def __init__(self, url):
-    self.url = url
-    self.browser = webdriver.Firefox()
-    self.browser.get(self.url)
-    self.writeFile()
-=======
   cursor = None
 
   def __init__(self, url):
     self.url = url
 
-    #self.browser = webdriver.Firefox()
-    #self.browser.get(self.url)
-    #self.writeFile()
+    options = Options()
+    options.headless = True
+    self.browser = webdriver.Firefox(options=options)
+    self.browser.get(self.url)
+    self.writeFile()
 
     self.conn = sqlite3.connect('database/verge_scrap.db')
     self.cursor = self.conn.cursor()
@@ -42,84 +30,10 @@ class scraper():
                             author TEXT,
                             date DATE)''')
 
->>>>>>> main
     self.readFile()
     self.getSoup()
 
   def writeFile(self):
-<<<<<<< HEAD
-    f = open("responsefile.html", "w")
-    f.write(self.browser.page_source)
-  
-  def readFile(self):
-    self.res = open("responsefile.html", "r").read()
-    
-  def soupRelativeAuthor(self, articleStarch):
-    
-    found = False
-
-    for parent in articleStarch.parents:
-      hrefrelsoup = parent.find_all('a')
-
-      for i in hrefrelsoup:
-        if(re.authorRegex(i.get('href'))):
-          print(i.get('href'))
-          found = True
-          input()
-        if(found):
-          break
-      
-      if(found):
-        break
-    
-    if not found:
-      print("author not found")
-          
-
-  def getSoup(self):
-    soup = bs4.BeautifulSoup(self.res, 'html.parser')
-
-    tag = input("Enter the tag format to find:")
-
-    starch = soup.find_all(tag)
-
-    links = []
-
-    for i in range(len(starch)):
-      hrefurl = starch[i].get('href')
-      nexthrefurl = starch[i+1].get('href')
-      
-      if(hrefurl == nexthrefurl):
-        continue
-      #print(hrefurl)
-      
-      if(re.nondatedUrlRegex(hrefurl)):
-        
-        '''if(re.datedUrlRegex(hrefurl)):
-          print('########## dated ARTICLE #######')
-        else:
-          print('########## ARTICLE #############')
-        '''
-
-        links.append(hrefurl)
-        print(starch[i].getText())
-        print(hrefurl)
-
-        self.soupRelativeAuthor(starch[i])
-
-        #print(starch[i].getText())
-        
-        
-        input()
-      
-      '''if(re.authorRegex(hrefurl)):
-        print(hrefurl)
-        for parent in starch[i].parents:
-          print(parent.name, end='->')
-        print()
-        input()'''
-
-=======
     date = datetime.now().strftime("%d%m%y")
     f = open(f"html_src_content/responsefile{date}.html", "w")
     f.write(self.browser.page_source)
@@ -141,10 +55,10 @@ class scraper():
     id = 0
 
     date = datetime.now().strftime("%d%m%y")
-    #file = open(f'csv/{date}_verge.csv', 'w')
+    file = open(f'csv/{date}_verge.csv', 'w')
     headers = ['id', 'URL', 'headline', 'author', 'date']
-    #writer = csv.DictWriter(file, fieldnames=headers)
-    #writer.writeheader()
+    writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
 
 
     for i in range(len(starch)):
@@ -173,16 +87,16 @@ class scraper():
 
         for a in authorStarch:
           author.append(a.get('content'))
+	
         author = ", ".join(author)
-
+        
         print('id: ' + str(id))
         print('url: ' + url)
         print('headline: ' + headline)
         print('author: ' + author)
         print('date: ' + date[:10])
         print()
-
-        '''
+        
         writer.writerow({
           'id': id,
           'URL': url,
@@ -195,12 +109,14 @@ class scraper():
                   (URL, headline, author, date)
                   VALUES(?, ?, ?, ?)''',
                   (url, headline, author, date))
-'''
+
     self.conn.commit()
     self.conn.close()
-
+    
+    print("\n***********************************************************")
+    print(datetime.now(ist).isoformat() + "\t 'Updated CSV and Database'")
+    print("***********************************************************\n")
         
->>>>>>> main
    
 if __name__ == "__main__":
   url = "http://theverge.com"
